2ba61e3be1a144795f8c92a86e7c66514211eed8
mahfuzsust <mahfuz@student.sust.edu>
2018-11-24 23:26:37 +0600

    Change port number to 4200

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ res.sendFile(path.join(__dirname+'/dist/' + package_json.name + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
