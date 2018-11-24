cb2183a08a531db442ca911bbbab5bf7c339f915
Mahfuzur Rahman <mahfuzur@augmedix.com>
2018-11-24 17:18:56 +0600

    Renamed angular application

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 const express = require('express');
 const path = require('path');
 
+var package_json = require('./package.json');
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LoginApp'));
+app.use(express.static(__dirname + '/dist/' + package_json.name));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/LoginApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/' + package_json.name + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
